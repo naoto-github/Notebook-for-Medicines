6efdb9ad34a8c78168ec777f2ea3279b2c3f5f59
Naoto Mukai <nmukai@sugiyama-u.ac.jp>
2015-11-15 11:22:44 +0900

    First Commit
    First Commit

---
+++ www/sheet.js
@@ -0,0 +1,8 @@
+//医療費明細書
+function Sheet(type, content, quantity, usage, uri){
+  this.type = type;
+  this.content = content;
+  this.quantity = quantity;
+  this.usage = usage;
+  this.uri = uri;
+}
